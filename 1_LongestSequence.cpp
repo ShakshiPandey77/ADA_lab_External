2c271b6bed18fd90d9e68e9dfb34669dd8db4ba6
ShakshiPandey77 <49754283+ShakshiPandey77@users.noreply.github.com>
2019-11-26 20:30:44 +0530

    Update 1_LongestSequence.cpp

--- 1_LongestSequence.cpp
+++ 1_LongestSequence.cpp
@@ -74,60 +74,58 @@ ANOTHER WAY:
 
 
 
-#include<iostream> 
-using namespace std; 
-
-void LCS( char *X, char *Y, int m, int n ) 
-  {
-   int L[m+1][n+1]; 
-   for (int i=0; i<=m; i++) 
-   { 
-     for (int j=0; j<=n; j++) 
-     { 
-       if (i == 0 || j == 0) 
-         L[i][j] = 0; 
-       else if (X[i-1] == Y[j-1]) 
-         L[i][j] = L[i-1][j-1] + 1; 
-       else
-         L[i][j] = max(L[i-1][j], L[i][j-1]); 
-     } 
-   } 
-   cout<<"length of LCS:"<<L[m][n]<<endl;
-   //printing lcs
-   int index = L[m][n]; 
-   char lcs[index+1]; 
-   lcs[index] = '\0'; // Set the terminating character 
-   int i = m, j = n; 
-   while (i > 0 && j > 0) 
-   { 
-      if (X[i-1] == Y[j-1]) 
-      { 
-          lcs[index-1] = X[i-1];  
-          i--; 
-          j--; 
-          index--;     
-      } 
-  
-      else if (L[i-1][j] > L[i][j-1]) 
-         i--; 
-      else
-         j--; 
-   } 
-  cout << "LCS of " << X << " and " << Y << " is " << lcs<<endl; 
-} 
-  
-int main() 
-{ 
-  // char X[] = "AGGTAB"; 
-  // char Y[] = "GXTXAYB";
-  char X[20],Y[20]; 
-  cout<<"enter the 1st string:";
-  cin>>X;
-  cout<<"enter the 2nd string:";
-  cin>>Y;
-  
-  int m = strlen(X); 
-  int n = strlen(Y); 
-  LCS(X, Y, m, n); 
-  return 0; 
-} 
+#include<iostream>
+#include<cstring>
+using namespace std;
+int max(int a,int b)
+{
+ return (a>b?a:b);
+}
+void lcs(string x,string y,int m,int n)
+{
+int l[m+1][n+1];
+for(int i=0;i<=m;i++)
+{
+for(int j=0;j<=n;j++)
+{
+if(i==0||j==0)
+l[i][j]=0;
+else if(x[i-1]==y[j-1])
+l[i][j]=l[i-1][j-1]+1;
+else
+l[i][j]=max(l[i-1][j],l[i][j-1]);
+}
+}
+cout<<"length of LCS:"<<l[m][n]<<endl;
+int index=l[m][n];
+char lcs[index+1];
+lcs[index]='\0';
+int i=m,j=n;
+while(i>0&&j>0)
+{
+if(x[i-1]==y[j-1])
+{
+lcs[index-1]=x[i-1];
+i--;
+j--;
+index--;
+}
+else if(l[i-1][j]>l[i][j-1])
+i--;
+else
+j--;
+}
+cout<<"longest common susequence of "<<x<<" and  "<<y<<" is "<<lcs<<endl;
+}
+int main()
+{
+string x,y;
+cout<<"enter the first string:";
+cin>>x;
+cout<<"enter the second string:";
+cin>>y;
+int m=x.length();
+int n=y.length();
+lcs(x,y,m,n);
+return 0;
+}
