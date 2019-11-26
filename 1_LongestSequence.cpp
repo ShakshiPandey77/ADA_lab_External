5fabdbf6d10c0bc8e544c439a8a695ae06e77e83
ShakshiPandey77 <49754283+ShakshiPandey77@users.noreply.github.com>
2019-11-26 20:31:49 +0530

    Update 1_LongestSequence.cpp

--- 1_LongestSequence.cpp
+++ 1_LongestSequence.cpp
@@ -1,3 +1,70 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+int max(int a,int b)
+{
+ return (a>b?a:b);
+}
+void lcs(string x,string y,int m,int n)
+{
+int l[m+1][n+1];
+for(int i=0;i<=m;i++)
+{
+for(int j=0;j<=n;j++)
+{
+if(i==0||j==0)
+l[i][j]=0;
+else if(x[i-1]==y[j-1])
+l[i][j]=l[i-1][j-1]+1;
+else
+l[i][j]=max(l[i-1][j],l[i][j-1]);
+}
+}
+cout<<"length of LCS:"<<l[m][n]<<endl;
+int index=l[m][n];
+char lcs[index+1];
+lcs[index]='\0';
+int i=m,j=n;
+while(i>0&&j>0)
+{
+if(x[i-1]==y[j-1])
+{
+lcs[index-1]=x[i-1];
+i--;
+j--;
+index--;
+}
+else if(l[i-1][j]>l[i][j-1])
+i--;
+else
+j--;
+}
+cout<<"longest common susequence of "<<x<<" and  "<<y<<" is "<<lcs<<endl;
+}
+int main()
+{
+string x,y;
+cout<<"enter the first string:";
+cin>>x;
+cout<<"enter the second string:";
+cin>>y;
+int m=x.length();
+int n=y.length();
+lcs(x,y,m,n);
+return 0;
+}
+
+
+
+
+
+ANOTHER WAY
+
+
+
+
+
+
 #include <iostream>
 #include <cstring>
 #include <cmath>
@@ -70,62 +137,8 @@ The Longest Common Subsequence is : bcvefgh
 
 
 
-ANOTHER WAY: 
 
 
 
-#include<iostream>
-#include<cstring>
-using namespace std;
-int max(int a,int b)
-{
- return (a>b?a:b);
-}
-void lcs(string x,string y,int m,int n)
-{
-int l[m+1][n+1];
-for(int i=0;i<=m;i++)
-{
-for(int j=0;j<=n;j++)
-{
-if(i==0||j==0)
-l[i][j]=0;
-else if(x[i-1]==y[j-1])
-l[i][j]=l[i-1][j-1]+1;
-else
-l[i][j]=max(l[i-1][j],l[i][j-1]);
-}
-}
-cout<<"length of LCS:"<<l[m][n]<<endl;
-int index=l[m][n];
-char lcs[index+1];
-lcs[index]='\0';
-int i=m,j=n;
-while(i>0&&j>0)
-{
-if(x[i-1]==y[j-1])
-{
-lcs[index-1]=x[i-1];
-i--;
-j--;
-index--;
-}
-else if(l[i-1][j]>l[i][j-1])
-i--;
-else
-j--;
-}
-cout<<"longest common susequence of "<<x<<" and  "<<y<<" is "<<lcs<<endl;
-}
-int main()
-{
-string x,y;
-cout<<"enter the first string:";
-cin>>x;
-cout<<"enter the second string:";
-cin>>y;
-int m=x.length();
-int n=y.length();
-lcs(x,y,m,n);
-return 0;
-}
+
+
